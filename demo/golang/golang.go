633206c5b85f46159f024f61b9ce7cc6725a7041
lufeewu <lufeewu@gmail.com>
2019-04-23 19:52:49 +0800

    add defer

--- demo/golang/golang.go
+++ demo/golang/golang.go
@@ -1,9 +1,11 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/sirupsen/logrus"
+)
 
 func testCap() {
-	// 为何 slice 可以 cap，map 为何不可以？
+	// 为何 slice 可以 cap, map 为何不可以？
 	// map 底层实现是什么？
 	var s []int
 	logrus.Infoln(cap(s))
@@ -22,10 +24,58 @@ func errNil() {
 
 	logrus.Infoln(trap == nil)
 	logrus.Infoln(trap != nil)
+}
+
+func testDefer() {
+	var i = 1
+	defer logrus.Infoln("i1=", i)
+	i++
+	defer func(i int) {
+		logrus.Infoln("i=", i)
+	}(i)
+}
+
+func testDefer2() {
+	// return 语句不是原子操作， 1. rval = xxx ,  2. ret，在这两步之间执行 defer
+	// defer 的底层由实现 deferproc 、deferreturn
+
+	f := func() int {
+		i := 5
+		defer func() {
+			i++
+		}()
+		return i
+	}()
+
+	f1 := func() (result int) {
+		defer func() {
+			result++
+		}()
+		return 0
+	}()
+
+	f2 := func() (result int) {
+		t := 5
+		defer func() {
+			t = t + 5
+		}()
+		return t
+	}()
+
+	f3 := func() (r int) {
+		defer func() {
+			r = r + 5
+		}()
+		return 1
+	}()
+
+	logrus.Infoln(f, f1, f2, f3)
 
 }
 
 func main() {
 	// errNil()
-	testCap()
+	// testCap()
+	// testDefer()
+	testDefer2()
 }

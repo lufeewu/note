7b55689dc35c80c63c08dbc9eec8ddb371ba6f95
lufeewu <lufeewu@gmail.com>
2021-08-23 14:58:39 +0800

    feat: leetcode 并查集

--- code/leetcode/684.冗余连接.go
+++ code/leetcode/684.冗余连接.go
@@ -6,32 +6,34 @@
 
 // @lc code=start
 func findRedundantConnection(edges [][]int) []int {
-	parent := make([]int, len(edges)+1)
-	for i := range parent {
-		parent[i] = i
+	var parents = make([]int, len(edges))
+	for i, _ := range parents {
+		parents[i] = i + 1
 	}
 
 	for _, e := range edges {
-		if !union(parent, e[0], e[1]) {
+		if union(parents, e[0], e[1]) == false {
 			return e
 		}
 	}
-	return nil
+
+	return []int{}
+
 }
 
-func find(parent []int, x int) int {
-	if parent[x] != x {
-		parent[x] = find(parent, parent[x])
+func find(parents []int, x int) int {
+	if parents[x-1] == x {
+		return x
 	}
-	return parent[x]
+	return find(parents, parents[x-1])
 }
 
-func union(parent []int, from, to int) bool {
-	x, y := find(parent, from), find(parent, to)
+func union(parents []int, from, to int) bool {
+	x, y := find(parents, from), find(parents, to)
 	if x == y {
-		return false
+		return false // uinon 失败, from to 已经是连通的
 	}
-	parent[x] = y
+	parents[x-1] = y
 	return true
 }
 

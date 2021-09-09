bccf265bfd5ad2fe966905698ccf4dc57c15117c
lufeewu <lufeewu@gmail.com>
2021-09-09 20:19:33 +0800

    feat: 排列

---
+++ code/leetcode/31.下一个排列.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=golang
+ *
+ * [31] 下一个排列
+ */
+
+// @lc code=start
+
+func nextPermutation(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := n - 1
+		for j >= 0 && nums[i] >= nums[j] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	reverse(nums[i+1:])
+}
+
+func reverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
+
+// @lc code=end
+

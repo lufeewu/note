9875b7c5658d403679eecb95dec14221afeac9ac
lufeewu <lufeewu@gmail.com>
2021-09-20 18:24:59 +0800

    feat: 增加两数之和

---
+++ code/leetcode/167.两数之和-ii-输入有序数组.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=golang
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+func twoSum(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+	for i < j {
+		if numbers[i]+numbers[j] == target {
+			return []int{i + 1, j + 1}
+		} else if numbers[i]+numbers[j] > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return []int{}
+}
+
+// @lc code=end
+

67a40e29a543543ce186a505ea65bda0c890b69c
lufeewu <lufeewu@gmail.com>
2021-09-24 20:46:23 +0800

    feat: 合并 k 个升序链表

---
+++ code/leetcode/23.合并k个升序链表.go
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=golang
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+	if len(lists) == 1 {
+		return lists[0]
+	}
+
+	i, j := 0, len(lists)-1
+	for j > 0 {
+		i = 0
+		for i < j {
+			lists[i] = mergeTwoList(lists[i], lists[j])
+			i++
+			j--
+		}
+	}
+	return lists[0]
+
+}
+
+func mergeTwoList(list1, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	var head, cur *ListNode
+	if list1.Val <= list2.Val {
+		head = list1
+		list1 = list1.Next
+	} else {
+		head = list2
+		list2 = list2.Next
+	}
+
+	cur = head
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		cur = cur.Next
+	}
+
+	if list1 != nil {
+		cur.Next = list1
+	} else {
+		cur.Next = list2
+	}
+
+	return head
+}
+
+// @lc code=end
+
